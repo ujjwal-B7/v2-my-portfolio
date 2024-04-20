ae0c3ac0942bf20eb40dcc146fc3a4db8cf30ef9
SLASH-7 <basnetujjwal98@gmail.com>
2024-04-20 21:55:31 +0545

    first push

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+const Nav = () => {
+  return (
+    <nav>
+      <section className="max-w-screen-xl mx-auto px-4 h-16 flex items-center justify-between">
+        <h1>hi</h1>
+        <div className="flex gap-4 list-none">
+          <li>hello</li>
+          <li>hello</li>
+          <li>hello</li>
+          <li>hello</li>
+          <li>hello</li>
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export default Nav;
