26a8613e8d02d341a81534a9cd996afb1ba8e9f5
SLASH-7 <basnetujjwal98@gmail.com>
2024-04-24 22:38:17 +0545

    project section complete

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,12 +2,15 @@ import React from "react";
 const About = () => {
   return (
     <section className="mb-32 flex list-none justify-center items-center">
-      <section className="xl:w-[60%] lg:w-[70%] md:w-[80%] w-full flex md:flex-row flex-col md:items-start items-center gap-10 lg:px-0 px-10 ">
+      <section className="xl:w-[60%] lg:w-[70%] md:w-[80%] w-full flex md:flex-row flex-col md:items-start items-center gap-10 lg:px-0 px-4">
         <div
           className="md:w-[60%] w-full md:px-0 space-y-4"
           data-aos="fade-right"
         >
-          <h2 className="mb-7 numbered-heading">About Me</h2>
+           <div className="flex">
+            <span className=" title-number">01.</span>
+            <h2 className="mb-7 numbered-heading">About Me</h2>
+          </div>
           <p>
             Hello! My name is{" "}
             <span className="highlighted-text">Ujjwal Basnet</span> . I am now
